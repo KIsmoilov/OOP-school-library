e316204aed0456e4af9c6570e95ce1cb911f3a52
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-02-03 18:16:38 +0500

    Introduce a bug

--- teacher.rb
+++ teacher.rb
@@ -6,7 +6,11 @@ class Teacher < Person
     @specialization = specialization
   end
 
-  def can_use_services?
+  def can_use_services??
     true
   end
 end
+
+teacher = Teacher.new('Web', 25, 'Khusniddin')
+result = teacher.can_use_services?
+puts result
