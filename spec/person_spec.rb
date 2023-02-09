10cb10c0d0b85139cbd397aac811684552bd74ee
TechnoAndy <47176316+TechnoAndy@users.noreply.github.com>
2023-02-09 08:41:13 +0200

    Update person_spec.rb

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -7,7 +7,7 @@ describe Person do
   context 'Unit test for person' do
     it 'should have the correct properties for person' do
       expect(@person.name).to eql('Eli')
-      expect(@person.age).to eql(5)
+      expect(@person.age).to eql(8)
     end
   end
 end
